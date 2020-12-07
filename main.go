b70335dc374ed82db1dbda3d124a699efbf22b99
María Cano <maria.cano4435@alumnos.udg.mx>
2020-12-06 21:02:17 -0600

    Listo, Terminado

--- main.go
+++ main.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"fmt"
-	//"time"
-	//"./Proceso"
 )
 
 func main() {
 	var c, id, p int
-	var imprimir bool
 	lista := ListaDeProcesos{}
 	id = 0
-	imprimir = false
 	for true {
 		fmt.Println("1) Agregar Proceso\n2)Mostrar Procesos Corriendo\n3)Eliminar Procesos Especificos\n4)Salir")
 		fmt.Scan(&c)
 		switch c {
 		case 1:
-			lista.AñadirProceso(Proceso{id,0})
+			lista.AñadirProceso(Proceso{id, 0})
 			id = id + 1
 			break
 		case 2:
 			imprimir = !imprimir
-			lista.MostrarProcesos(imprimir)
+			lista.MostrarProcesos()
 			break
 		case 3:
 			fmt.Println("Who?")
--- proceso.go
+++ proceso.go
@@ -1,39 +1,56 @@
 package main
 
-import(
+import (
 	"fmt"
+	"time"
 )
+
+var imprimir = false
+
 type Proceso struct {
-	Id int
-	Contador int 
+	Id       int
+	Contador int
 }
-type ListaDeProcesos struct{
+type ListaDeProcesos struct {
 	Lista []Proceso
 }
 
-func (p *Proceso) MostrarProceso(b bool){
-	if b {
-		fmt.Println( p.Id,":",p.Contador)
+func (p *Proceso) MostrarProceso() {
+	for {
+		fmt.Println(p.Id, ":", p.Contador)
+		time.Sleep(time.Millisecond * 500)
+		if !imprimir {
+			return
+		}
 	}
 }
-func (l *ListaDeProcesos) AñadirProceso(p Proceso){
-	l.Lista= append(l.Lista,p)
+func (p *Proceso) hacerPreceso() {
+	for {
+		p.Contador += 1
+		time.Sleep(time.Millisecond * 500)
+	}
 }
-func (l *ListaDeProcesos) MostrarProcesos(b bool){
-	
-	for i:= 0; i<len(l.Lista);i++{
-		go l.Lista[i].MostrarProceso(b)
+func (l *ListaDeProcesos) AñadirProceso(p Proceso) {
+	l.Lista = append(l.Lista, p)
+
+	for i := 0; i < len(l.Lista); i++ {
+		go l.Lista[i].hacerPreceso()
+	}
+}
+func (l *ListaDeProcesos) MostrarProcesos() {
+
+	for i := 0; i < len(l.Lista); i++ {
+		go l.Lista[i].MostrarProceso()
 	}
 
 }
 
-func (l *ListaDeProcesos) remove(p int){
-	l1:= ListaDeProcesos{}
-	for i:= 0;i<len(l.Lista);i++{
-		if l.Lista[i].Id!=p{
+func (l *ListaDeProcesos) remove(p int) {
+	l1 := ListaDeProcesos{}
+	for i := 0; i < len(l.Lista); i++ {
+		if l.Lista[i].Id != p {
 			l1.AñadirProceso(l.Lista[i])
 		}
-		
 	}
-	l= &l1
+	*l = l1
 }
